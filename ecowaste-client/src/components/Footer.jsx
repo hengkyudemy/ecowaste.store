737b80f9250361727b56e76cf7debd3ee64d6182
Fadel18 <fbukhori8@gmail.com>
2025-07-16 09:12:25 +0700

    Add Footer, Header, Sidebar

---
+++ ecowaste-client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+
+const Footer = () => {
+  return (
+    <footer className="eco-footer">
+      <p>copyright@ecowaste.store 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
